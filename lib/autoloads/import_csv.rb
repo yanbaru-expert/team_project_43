cd70087b936e1df498e42305edfc7d189fb08f33
imaizumi <the.sea.treaders@gmail.com>
2021-02-12 23:09:42 +0900

    テキスト教材のCSVインポート機能を実装

---
+++ lib/autoloads/import_csv.rb
@@ -0,0 +1,14 @@
+
+class ImportCsv
+  #csvデータのパスを引数で受け取り、インポート処理を実行
+  def self.textimport(path)
+    CSV.foreach(path, headers: true) do |row|
+      Text.create!(
+        genre: row["genre"],
+        title: row["title"],
+        content: row["content"]
+      )
+    end
+    puts "TEXTへCSVデータ投入完了"
+  end
+end
