dc2145c64b365df55fe4b5285cb7650b9df7d841
ao21 <lpubnrm.ay@gmail.com>
2021-02-15 13:27:56 +0900

    修正:seedsファイルの記述位置を変更

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,14 +6,18 @@ User.find_or_create_by!(email: EMAIL) do |user|
   puts 'ユーザーの初期データインポートに成功しました。'
 end
 
+ADMIN_EMAIL = 'admin@example.com'
+ADMIN_PASSWORD = 'password'
+
+AdminUser.find_or_create_by!(email: ADMIN_EMAIL) do |admin_user|
+  admin_user.password = ADMIN_PASSWORD
+  admin_user.password_confirmation = ADMIN_PASSWORD
+  puts '管理者の初期データインポートに成功しました。'
+end
+
+
 Text.destroy_all
 ImportCsv.text_import("db/csv_data/text_data.csv")
 
 Movie.destroy_all
 ImportCsv.movie_import("db/csv_data/movie_data.csv")
-
-AdminUser.find_or_create_by!(email: 'admin@example.com') do |admin_user|
-  admin_user.password = 'password'
-  admin_user.password_confirmation = 'password'
-  puts '管理者の初期データインポートに成功しました。'
-end
