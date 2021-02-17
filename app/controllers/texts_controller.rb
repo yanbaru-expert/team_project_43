a5d776eaaf6c8b45c734c63a624c3a0e17ab7de9
ao21 <lpubnrm.ay@gmail.com>
2021-02-17 11:20:43 +0900

    textsコントローラの index を編集

--- app/controllers/texts_controller.rb
+++ app/controllers/texts_controller.rb
@@ -1,5 +1,6 @@
 class TextsController < ApplicationController
   def index
-
+    genre_rails = ["Basic", "Git", "HTML&CSS", "Ruby", "Ruby on Rails"]
+    @texts = Text.where(genre: genre_rails)
   end
 end
