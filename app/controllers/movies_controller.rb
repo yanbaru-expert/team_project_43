cf678a2777b081b4ef4dbdbb7c10846fde56a3ea
imaizumi <the.sea.treaders@gmail.com>
2021-02-18 09:31:29 +0900

    bootstrapでスタイルを修正

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -1,5 +1,5 @@
 class MoviesController < ApplicationController
   def index
-    @movies = Movie.all
+    @movies = Movie.where(genre: ["Basic", "Git", "HTML&CSS", "Ruby", "Ruby on Rails"])
   end
 end
