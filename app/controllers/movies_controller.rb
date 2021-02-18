90d6effe63baf9edb755fd72411324a0aa1cfce2
imaizumi <the.sea.treaders@gmail.com>
2021-02-18 20:54:43 +0900

    コントローラーでテーブルのidを昇順に並び替え

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -1,5 +1,5 @@
 class MoviesController < ApplicationController
   def index
-    @movies = Movie.where(genre: ["Basic", "Git", "HTML&CSS", "Ruby", "Ruby on Rails"])
+    @movies = Movie.where(genre: ["Basic", "Git", "HTML&CSS", "Ruby", "Ruby on Rails"]).order(id: :asc)
   end
 end
