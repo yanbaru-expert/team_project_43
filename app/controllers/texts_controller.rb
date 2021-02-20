ede309806cb17f5489869f8bc3320654bdd76d88
ao21 <lpubnrm.ay@gmail.com>
2021-02-20 22:11:00 +0900

    一覧ページに読破済み機能を実装

--- app/controllers/texts_controller.rb
+++ app/controllers/texts_controller.rb
@@ -2,6 +2,7 @@ class TextsController < ApplicationController
   def index
     genre_rails = ["Basic", "Git", "HTML&CSS", "Ruby", "Ruby on Rails"]
     @texts = Text.where(genre: genre_rails).order("id")
+    @read_text_ids = current_user.reads.pluck(:text_id)
   end
 
   def show
