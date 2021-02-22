e434bf7505e285800f1c7d43a52c8e19feb7acb5
gorgonzola731 <nk.kenken31@gmail.com>
2021-02-22 11:00:09 +0900

    モデルに関連づけとバリデーションを追加

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -1,4 +1,5 @@
 class Movie < ApplicationRecord
+  has_many :watcheds, dependent: :destroy
   validates :genre, presence: true
   validates :title, presence: true
   validates :url, presence: true
--- app/models/watched.rb
+++ app/models/watched.rb
@@ -1,4 +1,6 @@
 class Watched < ApplicationRecord
   belongs_to :user
   belongs_to :movie
+
+  validates :user_id, uniqueness: { scope: :movie_id }
 end
