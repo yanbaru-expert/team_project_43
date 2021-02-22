b78c4b722c7b40336f114a4e8488562263c95ef1
gorgonzola731 <nk.kenken31@gmail.com>
2021-02-22 13:02:55 +0900

    視聴済み機能を非同期化

--- app/controllers/watcheds_controller.rb
+++ app/controllers/watcheds_controller.rb
@@ -1,11 +1,11 @@
 class WatchedsController < ApplicationController
   def create
-    current_user.watcheds.create!(movie_id: params[:movie_id])
-    redirect_back(fallback_location: root_path)
+    @movie = Movie.find(paramas[:movie_id])
+    current_user.watcheds.create!(movie_id: @movie.id)
   end
 
   def destroy
-    current_user.watcheds.find_by(movie_id: params[:movie_id]).destroy!
-    redirect_back(fallback_location: root_path)
+    @movie = Movie.find(paramas[:movie_id])
+    current_user.watcheds.find_by(movie_id: @movie.id).destroy!
   end
 end
