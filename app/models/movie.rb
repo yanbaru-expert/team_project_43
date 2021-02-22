0df9df3e80e570613303fd6588cce540b6da5a37
gorgonzola731 <nk.kenken31@gmail.com>
2021-02-22 14:19:29 +0900

    watched_byメソッドをmovie.rbに移動

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -3,4 +3,8 @@ class Movie < ApplicationRecord
   validates :genre, presence: true
   validates :title, presence: true
   validates :url, presence: true
+
+  def watched_by?(user)
+    watcheds.find_by(user_id: user.id).present?
+  end
 end
