99bf3efdacd4ca827948ac2f1a6fee03c8ac2b3b
ao21 <lpubnrm.ay@gmail.com>
2021-02-26 15:55:50 +0900

    Userモデルに関連付けを追加

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,9 @@
 class User < ApplicationRecord
   has_many :reads, dependent: :destroy
+
+  # 読破済みのテキストを取得
+  has_many :read_texts, through: :reads, source: :text
+
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
