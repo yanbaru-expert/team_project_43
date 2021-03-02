9248961ed8ff317609112408999855916c6be73b
gorgonzola731 <nk.kenken31@gmail.com>
2021-03-02 18:15:54 +0900

    sessionsコントローラを作成、ゲストログイン機能の実装

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,9 @@ Rails.application.routes.draw do
   devise_for :admin_users, ActiveAdmin::Devise.config
   ActiveAdmin.routes(self)
   devise_for :users
+  devise_scope :user do
+    post 'users/guest_sign_in', to: 'users/sessions#new_guest'
+  end
   root to: 'texts#index'
   resources :texts, only: [:index, :show] do
     resource :reads, only: [:create, :destroy]
