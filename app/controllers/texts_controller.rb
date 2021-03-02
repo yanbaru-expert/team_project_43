58df3d7b8d7f6d16bf75ea6d6e1b7c3dd570ef01
imaizumi <the.sea.treaders@gmail.com>
2021-03-02 17:23:09 +0900

    ヘッダーのリンク部分の修正とコントローラーの記述を修正

--- app/controllers/texts_controller.rb
+++ app/controllers/texts_controller.rb
@@ -1,11 +1,10 @@
 class TextsController < ApplicationController
   def index
     genre_rails = ["Basic", "Git", "HTML&CSS", "Ruby", "Ruby on Rails"]
-    genre_php = ["PHP"]
-    if params[:genre]
+    if params[:genre] == nil
       @texts = Text.where(genre: genre_rails).order("id")
     else
-      @texts = Text.where(genre: genre_php).order("id")
+      @texts = Text.where(genre: params[:genre])
     end
     @read_text_ids = current_user.reads.pluck(:text_id)
   end
