4177d7d703ba735bd008f096179689fb8461658c
imaizumi <the.sea.treaders@gmail.com>
2021-03-02 14:59:56 +0900

    コントローラにRailsとPHP教材の条件分岐を記述

--- app/controllers/texts_controller.rb
+++ app/controllers/texts_controller.rb
@@ -1,7 +1,12 @@
 class TextsController < ApplicationController
   def index
     genre_rails = ["Basic", "Git", "HTML&CSS", "Ruby", "Ruby on Rails"]
-    @texts = Text.where(genre: genre_rails).order("id")
+    genre_php = ["PHP"]
+    if params[:genre]
+      @texts = Text.where(genre: genre_rails).order("id")
+    else
+      @texts = Text.where(genre: genre_php).order("id")
+    end
     @read_text_ids = current_user.reads.pluck(:text_id)
   end
 
