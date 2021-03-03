f3d8e3ca859e744ca96ac6fc68bdf3b25472d7d1
ao21 <lpubnrm.ay@gmail.com>
2021-03-03 12:13:36 +0900

    parameter カラムにDB制約とバリデーションを追加

--- app/models/genre.rb
+++ app/models/genre.rb
@@ -1,3 +1,4 @@
 class Genre < ApplicationRecord
+  validates :parameter, presence: true
   validates :title, presence: true
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_03_03_023943) do
+ActiveRecord::Schema.define(version: 2021_03_03_031016) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -42,7 +42,7 @@ ActiveRecord::Schema.define(version: 2021_03_03_023943) do
   end
 
   create_table "genres", force: :cascade do |t|
-    t.string "parameter"
+    t.string "parameter", null: false
     t.string "title", null: false
     t.string "color"
     t.datetime "created_at", precision: 6, null: false
