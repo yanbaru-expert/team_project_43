ee454fd97b3431b63603928d05488d354686edbf
ao21 <lpubnrm.ay@gmail.com>
2021-03-04 17:07:30 +0900

    進捗管理のデータを配列に加工するメソッドを Genre モデルに追加

--- app/models/genre.rb
+++ app/models/genre.rb
@@ -1,4 +1,30 @@
 class Genre < ApplicationRecord
   validates :parameter, presence: true
   validates :title, presence: true
+
+  # 進捗管理のデータをビューで取り出しやすいように配列に加工
+  def self.progress_data(user)
+    counts = Text.total_count
+    read_counts = Text.read_count(user)
+    genres = Genre.all
+    progress_data = {}
+
+    genres.each do |genre|
+      total_count = counts[genre.parameter] || 0
+      read_count = read_counts[genre.parameter] || 0
+      count = "#{read_count}/#{total_count}"
+      percentage = Text.percentage(read_count, total_count)
+
+      progress_data.store(
+        genre.parameter,
+          {
+            title: genre.title,
+            color: genre.color,
+            count: count,
+            percentage: percentage
+        }
+      )
+    end
+    return progress_data
+  end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -6,15 +6,4 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
-
-
-  # 読破済みの数を配列で取得
-  def genres_read_count
-    self.read_texts.group(:genre).count
-  end
-
-  # 読破済み進捗のパーセンテージ
-  def self.percentage(genre_read_count, genre_count)
-    ( genre_read_count.to_f / genre_count.to_f  * 100.0 ).to_i
-  end
 end
