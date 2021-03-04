5959613f98e47d37604701c4e41298591cf589bc
ao21 <lpubnrm.ay@gmail.com>
2021-03-04 12:08:04 +0900

    genres テーブルにデータを追加するマイグレーション

---
+++ db/migrate/20210304013614_insert_initial_genres.rb
@@ -0,0 +1,38 @@
+class InsertInitialGenres < ActiveRecord::Migration[6.1]
+  def change
+    genres = [
+      {
+        parameter: "Basic",
+        title: "Basic",
+        color: "#007bff"
+      },
+      {
+        parameter: "Git",
+        title: "Git",
+        color: "#343a40"
+      },
+      {
+        parameter: "Ruby",
+        title: "Ruby",
+        color: "#dc3545"
+      },
+      {
+        parameter: "Ruby on Rails",
+        title: "Ruby on Rails",
+        color: "#dc3545"
+      },
+      {
+        parameter: "Php",
+        title: "PHP",
+        color: "#6f42c1"
+      },
+      {
+        parameter: "AWS",
+        title: "AWS",
+        color: "#ec7211"
+      }
+    ]
+
+    Genre.create!(genres)
+  end
+end
