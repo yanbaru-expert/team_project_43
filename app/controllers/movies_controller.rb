1f0576574ac0d755c2f9c468c440de8c8dcc1105
imaizumi <the.sea.treaders@gmail.com>
2021-03-06 18:05:24 +0900

    コントローラーにRuby／RailsとPHPの動画教材の条件分岐を追加

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -1,7 +1,11 @@
 class MoviesController < ApplicationController
   PER_PAGE = 6
   def index
-    rails_movies = ["Basic","Git", "HTML&CSS", "Ruby", "Ruby on Rails"]
-    @movies = Movie.where(genre: rails_movies).order(id: :asc).page(params[:page]).per(PER_PAGE)
+    rails_movies = ["Basic", "Git", "HTML&CSS", "Ruby", "Ruby on Rails"]
+    if params[:genre] == nil
+      @movies = Movie.where(genre: rails_movies).order(id: :asc).page(params[:page]).per(PER_PAGE)
+    else
+      @movies = Movie.where(genre: params[:genre]).order(id: :asc).page(params[:page]).per(PER_PAGE)
+    end
   end
 end
