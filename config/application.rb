f59b4b72ef7f115aa203341a1039e6c4c3fdc903
zenfumi <ys00878@gmail.com>
2021-03-09 09:41:43 +0900

    Fakerによるダミーデータを作成する記述に修正

--- config/application.rb
+++ config/application.rb
@@ -37,6 +37,6 @@ module TeamProject
 
     # Don't generate system test files.
     config.generators.system_tests = nil
-    config.factory_bot.definition_file_paths = ["spec/factories"] 
+    config.factory_bot.definition_file_paths = ["spec/factories"]
   end
 end
