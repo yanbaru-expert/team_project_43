0988cecacb177cfd900964ed63218c9c623c9075
gorgonzola731 <nk.kenken31@gmail.com>
2021-03-09 12:46:47 +0900

    ゲストユーザーのパスワードを変更不可にする

--- config/routes.rb
+++ config/routes.rb
@@ -2,9 +2,10 @@ Rails.application.routes.draw do
   devise_for :admin_users, ActiveAdmin::Devise.config
   ActiveAdmin.routes(self)
   devise_for :users, controllers: {
-    registrations: 'users/registrations'
+    registrations: 'users/registrations',
+    passwords: 'users/passwords'
   }
-  
+
   devise_scope :user do
     post 'users/guest_sign_in', to: 'users/sessions#new_guest'
   end
