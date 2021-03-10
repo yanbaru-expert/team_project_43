c743e6ddc3aa4c3e566610d4874b8aee63dc8179
gorgonzola731 <nk.kenken31@gmail.com>
2021-03-10 17:52:01 +0900

    check_guestメソッドをapplicationコントローラにまとめる

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -1,10 +1,3 @@
 class Users::RegistrationsController < Devise::RegistrationsController
   before_action :check_guest, only: %i[update destroy]
-
-  def check_guest
-    if resource.email == 'guest@example.com'
-      redirect_to root_path, alert: 'ゲストユーザーの変更・削除はできません。'
-    end
-  end
-
 end
